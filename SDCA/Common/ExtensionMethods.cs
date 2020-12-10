483eca8369ebbc6d8b81c2ddf71322a6b031c489
Danfer <dhabed@outlook.com>
2020-12-10 14:10:20 -0600

    Add project files.

---
+++ SDCA/Common/ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SDCA.Common
+{
+    public static class ExtensionMethods
+    {
+        public static string[] ToPropertyList<T>(this Type objType, string labelName)
+            => objType.GetProperties().Where(x => x.Name != labelName)
+            .Select(x => x.Name).ToArray();
+    }
+}
---
+++ SDCA/ML/Objects/EmploymentHistoryPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDCA.ML.Objects
+{
+    public class EmploymentHistoryPrediction
+    {
+        [ColumnName("Score")]
+        public float DurationInMonths;
+    }
+}
